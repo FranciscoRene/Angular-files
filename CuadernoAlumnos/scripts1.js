d0fcaecd35a44fac27b81a25c2de6f2c1718e768
Francisco Flores <franciscorenef@gmail.com>
2016-01-18 21:59:58 -0300

    Aplicación básica: Cuaderno de Alumnos

---
+++ CuadernoAlumnos/scripts1.js
@@ -0,0 +1,7 @@
+function AlumnosController($scope){
+	$scope.alumnos=[
+		{nombre:"alumno 1", telefono:"telefono 1", curso:"curso 1"},
+		{nombre:"alumno 2", telefono:"telefono 2", curso:"curso 2"},
+		{nombre:"alumno 3", telefono:"telefono 3", curso:"curso 3"},
+	];
+}
